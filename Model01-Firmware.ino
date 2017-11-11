3e184ebbd2f2a59513b784c8b3df3b071cc7052a
Thomas Sibley <tom@zulutango.org>
2017-11-11 12:46:36 -0800

    Map Prog to Escape
    The finger memory for this one is strong!  and the default Escape key is
    not usefully placed for me.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -127,7 +127,7 @@ enum { QWERTY, FUNCTION, NUMPAD }; // layers
 const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [QWERTY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+  (Key_Escape,   Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Tab,      Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,

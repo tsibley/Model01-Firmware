365f7df046154b5e1804890ed6b24427ac3d68eb
Thomas Sibley <trsibley@uw.edu>
2017-11-21 09:19:58 -0800

    Try out swapping Backspace/Delete and Left Control
    While both are used a lot, I think I'm going to prefer having a modifier
    key under my natural thumb position instead of a destructive key.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -131,7 +131,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_Tab,      Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftShift, Key_LeftGui,
+   Key_Backspace, Key_LeftControl, Key_LeftShift, Key_LeftGui,
    ShiftToLayer(FUNCTION),
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_KeypadNumLock,
@@ -146,7 +146,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_Backtick, ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
    Key_Home,     Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
    Key_End,      Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
-   ___, Key_Delete, ___, ___,
+   Key_Delete, ___, ___, ___,
    ___,
 
    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,

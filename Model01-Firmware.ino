6b7eea49e29ac4017a4e420170111893277fbeeb
Thomas Sibley <trsibley@uw.edu>
2017-12-13 09:27:14 -0800

    Revert "Experiment with removing the Numpad layer and using the key as Backspace/Delete"
    This reverts commit 3ea64e7bd912c6c97377023cf61584056f37f996 and
    573f46e3907ac5f39406da545b05529f9c770909.
    
    This doesn't really work out well.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -22,6 +22,9 @@
 // Support for controlling the keyboard's LEDs
 #include "Kaleidoscope-LEDControl.h"
 
+// Support for "Numpad" mode, which is mostly just the Numpad specific LED mode
+#include "Kaleidoscope-NumPad.h"
+
 // Support for an "LED off mode"
 #include "LED-Off.h"
 
@@ -103,7 +106,7 @@ enum { MACRO_VERSION_INFO,
   *
   */
 
-enum { QWERTY, FUNCTION }; // layers
+enum { QWERTY, NUMPAD, FUNCTION }; // layers
 
 /* This comment temporarily turns off astyle's indent enforcement
  *   so we can make the keymaps actually resemble the physical key layout better
@@ -120,25 +123,39 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_LeftControl, Key_LeftGui, Key_LeftShift, Key_Backspace,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Backspace,
+   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
    Key_LeftAlt, Key_RightShift, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
+  [NUMPAD] =  KEYMAP_STACKED
+  (___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___,
+   ___,
+
+   M(MACRO_VERSION_INFO),  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___,
+   ___,                    ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___,
+                           ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         Key_Quote,
+   ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
+   ___, ___, ___, ___,
+   ___),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (M(MACRO_VERSION_INFO),  Key_F1,           Key_F2,      Key_F3, Key_F4, Key_F5, XXX,
-   Key_Backtick,           ___,              ___,         ___,    ___,    ___,    ___,
-   Key_Home,               ___,              ___,         ___,    ___,    ___,
-   Key_End,                Key_PrintScreen,  Key_Insert,  ___,    ___,    ___,    ___,
+  (___,          Key_F1,           Key_F2,      Key_F3, Key_F4, Key_F5, XXX,
+   Key_Backtick, ___,              ___,         ___,    ___,    ___,    ___,
+   Key_Home,     ___,              ___,         ___,    ___,    ___,
+   Key_End,      Key_PrintScreen,  Key_Insert,  ___,    ___,    ___,    ___,
    ___, ___, ___, Key_Delete,
    ___,
 
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_Delete,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F11,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              Key_F12,
+   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
+   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
+                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
    Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
    ___, ___, Key_Enter, ___,
    ___)
@@ -256,9 +273,17 @@ void setup() {
     // The stalker effect lights up the keys you've pressed recently
     &StalkerEffect,
 
+    // The numpad plugin is responsible for lighting up the 'numpad' mode
+    // with a custom LED effect
+    &NumPad,
+
     // The macros plugin adds support for macros
     &Macros
   );
+
+  // While we hope to improve this in the future, the NumPad plugin
+  // needs to be explicitly told which keymap layer is your numpad layer
+  NumPad.numPadLayer = NUMPAD;
 
   // We set the brightness of the rainbow effects to 150 (on a scale of 0-255)
   // This draws more than 500mA, but looks much nicer than a dimmer effect

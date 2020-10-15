ca6e05c9380ed92b613d4b6426c1d05142ae3940
Thomas Sibley <tsibley@fredhutch.org>
2020-10-15 12:33:19 -0700

    Replace NumLock with a key to lock the computer instead
    I never use NumLock but I do hit it accidentally.  On the other hand, I
    always lock my computer when walking away, and this makes it one easy
    keystroke.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -208,7 +208,7 @@ KEYMAPS(
    Key_LeftAlt, Key_LeftControl, Key_LeftShift, Key_LeftGui,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
+   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Consumer_AL_TerminalLockSlashScreensaver,
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
